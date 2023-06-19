4700044074e390c4113b9a17b32e5cccdbe3e282
Francisco das Chagas Silva Junior <fcsjr.chagas@gmail.com>
2023-06-18 22:00:49 -0400

    Set up as NPM module

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
+module.exports = Phrase;
+
 // Reverses a string.
 String.prototype.reverse = function reverse() {
   return Array.from(this).reverse().join("");
-}
-
-String.prototype.blank = function blank() {
-  return !!this.match(/^\s*$/);
-}
-
-Array.prototype.last = function last() {
-  let last_index = this.length - 1
-  return this[last_index]
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function processor(string) {
-    return string.toLowerCase()
-  }
-
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.content.toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
